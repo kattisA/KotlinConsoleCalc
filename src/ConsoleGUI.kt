e16afa11b03f973fa49f9325d26075409008039d
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2020-01-13 23:19:21 +0100

    First simple menu with IO

--- src/ConsoleGUI.kt
+++ src/ConsoleGUI.kt
@@ -1,4 +1,25 @@
 fun main(){
-    println("Hello World")
+    var userinput = 0
+    println("Vad heter du?")
+    var firstName = readLine()
+    println("Tjenare  $firstName!")
+
+    do {
+        println("1. Räkna tid")
+        println("2. Räkna lön")
+        println("3. Avsluta")
+        userinput = readLine()?.toInt()!!
+
+        when(userinput){
+            1 -> {
+                println("Du valde ett")
+                return }
+            2 -> {println("Du valde två")
+            return}
+            3 -> { println("Hej då $firstName")
+            return}
+            else -> println("Gör ett giltigt val")
+        }
+    } while (userinput != 3)
 }
 
