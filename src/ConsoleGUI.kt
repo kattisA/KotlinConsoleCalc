bd6b36760f7886ee65188a07e9e6644990b30c86
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2020-02-19 16:04:45 +0100

    New class for the first calculations of time and reading multiple input

--- src/ConsoleGUI.kt
+++ src/ConsoleGUI.kt
@@ -1,27 +1,31 @@
 fun main(){
-    var userinput = 0
+    var userInput = 0
     println("Vad heter du?")
     var firstName = readLine()
     println("Tjenare  $firstName!")
 
     var logic = Logic(firstName.toString())
 
+
     do {
         println("1. Räkna tid")
         println("2. Räkna lön")
-        println("3. Avsluta")
-        userinput = readLine()?.toInt()!!
+        println("3. Räkna ut ålder")
+        println("4. Avsluta")
+        userInput = readLine()?.toInt()!!
 
-        when(userinput){
+        when(userInput){
             1 -> {
-                println("Du valde ett")
+                timeStampGUI(firstName)
                 return }
             2 -> {println("Du valde två")
             return}
-            3 -> { println("Hej då $firstName")
+            3 -> { println("Du valde tre")
+            return}
+            4 -> { println("Hej då $firstName")
             return}
             else -> println("Gör ett giltigt val")
         }
-    } while (userinput != 3)
+    } while (userInput != 3)
 }
 
---
+++ src/TimeStampsGUI.kt
@@ -0,0 +1,6 @@
+fun timeStampGUI(firstName:String?){
+    println("Tjenare $firstName i timeStamp ange två klockslag")
+    val(first, second ) = readLine()!!.split(' ')
+    println("$first $second")
+    return
+}
