7ac00745c109e621830d86a92d19bcf6d57e6251
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2020-02-24 23:28:14 +0100

    Making a Test module for all unit tests

---
+++ Test/ValidationKtTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+internal class ValidationKtTest{
+    @Test
+    internal fun testSomething() {
+        TODO("Write some validation tests!") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
