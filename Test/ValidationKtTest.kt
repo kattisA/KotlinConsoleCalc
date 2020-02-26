26d4fe7b3e1e3e763c13fa52d51cabc08cb55673
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2020-02-26 19:37:23 +0100

    First test for wrong input

--- Test/ValidationKtTest.kt
+++ Test/ValidationKtTest.kt
@@ -1,9 +1,18 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertNotEquals
 
 internal class ValidationKtTest{
     @Test
-    internal fun testSomething() {
-        TODO("Write some validation tests!") //To change body of created functions use File | Settings | File Templates.
+    internal fun `wrong user input returns false`() {
+        assertNotEquals(
+            true,
+            validateInputIsTimeStamps(arrayOf ("adr", "3445"))
+        )
+    }
+
+    @Test
+    internal fun `wrong timestamps returns false`() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 
