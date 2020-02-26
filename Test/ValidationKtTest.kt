37d17f398c3efc78055c97dcc436242694cd10e7
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2020-02-26 22:32:43 +0100

    Changed to parametrized test for multiple input

--- Test/ValidationKtTest.kt
+++ Test/ValidationKtTest.kt
@@ -1,18 +1,25 @@
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertNotEquals
 
 internal class ValidationKtTest{
-    @Test
-    internal fun `wrong user input returns false`() {
+    @ParameterizedTest
+    @ValueSource(strings= ["adr", "3455","&%¤/"])
+    internal fun `wrong user input returns false`(incorrectInput: String) {
         assertNotEquals(
             true,
-            validateInputIsTimeStamps(arrayOf ("adr", "3445"))
+            validateInputIsTimeStamps(arrayOf (incorrectInput))
         )
     }
 
-    @Test
-    internal fun `wrong timestamps returns false`() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    @ParameterizedTest
+    @ValueSource(strings = ["34:00", "12:99", "1:00"])
+    internal fun `wrong timestamps returns false`(incorrectTimeStamps: String) {
+        assertNotEquals(
+            true,
+            validateInputIsTimeStamps(arrayOf(incorrectTimeStamps)))
+
     }
 }
 
--- src/Validation.kt
+++ src/Validation.kt
@@ -1,6 +1,6 @@
 
 
 fun validateInputIsTimeStamps(inputs: Array<String>): Boolean? {
-    return inputs.all { it.matches(Regex("[0-2][0-9]:[0-9][0-9]")) }
+    return inputs.all { it.matches(Regex("(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]")) }
 
 }
